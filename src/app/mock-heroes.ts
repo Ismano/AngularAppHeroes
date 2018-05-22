05b0f04a5efd073ac5c9f6b8fedb865ceed6a687
Ismano <ismano37@gmail.com>
2018-05-22 10:07:43 +0200

    Primera pujada
    Pujada tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id : 1 , name : "Superman"},
+  { id: 2, name: "Spidercerdo" },
+  { id: 3, name: "Harrypopotter" },
+  { id: 4, name: "Ohtmaneee" },
+  { id: 5, name: "C.Tangana" },
+  { id: 6, name: "¡Ohh no meatropella!" },
+  { id: 7, name: "Sir Archibald Percival" },
+  { id: 8, name: "Elvis Canario" },
+  { id: 9, name: " Mrs. Chaman" },
+  { id: 10, name: " Rancius " },
+  { id: 11, name: " Deadpoll " },
+];
